8ae6b9f64edc4d5a64d802c3634f9b9d1a2038cb
APPLE <apple@APPLEs-MacBook-Air.local>
2019-12-27 13:05:32 +0530

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'assign3';
+  oddNumbers: number[] = [];
+  evenNumbers: number[] = [];
+
+  onIntervalFired(firedNumber: number) {
+  if (firedNumber%2 == 0) {
+    this.evenNumbers.push(firedNumber );
+  }
+  else {
+    this.oddNumbers.push(firedNumber);
+  }
+  }
 }
